75644f5fff4a9bd730f7849dae74e0ef8e5fc977
dubidubab1234 <lsbmoon78@gmail.com>
2022-06-30 20:33:36 +0900

    Make lines on Canvas

---
+++ app.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById("jsCanvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+context.strokeStyle = "black";
+context.lineWidth = 3;
+context.lineCap = "round";
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!painting) {
+    context.beginPath();
+    context.moveTo(x, y);
+  } else {
+    context.lineTo(x, y);
+    context.stroke();
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
